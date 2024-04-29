7064bba9a9f7d50cd1d2b976fbec9faf29caec5b
Daniel Frey <dmfrey@gmail.com>
2024-04-29 13:45:12 -0400

    register more liquibase hints

--- src/main/java/com/broadcom/springconsulting/timeservice/currentTime/adapters/out/persistence/DatabaseConfig.java
+++ src/main/java/com/broadcom/springconsulting/timeservice/currentTime/adapters/out/persistence/DatabaseConfig.java
@@ -1,9 +1,13 @@
 package com.broadcom.springconsulting.timeservice.currentTime.adapters.out.persistence;
 
+import liquibase.changelog.ChangeLogHistoryServiceFactory;
+import org.springframework.aot.hint.ExecutableMode;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportRuntimeHints;
+
+import java.util.Collections;
 
 @Configuration
 @ImportRuntimeHints( DatabaseConfig.LiquibaseRuntimeHints.class )
@@ -14,6 +18,9 @@ public class DatabaseConfig {
 
         @Override
         public void registerHints( RuntimeHints hints, ClassLoader classLoader ) {
+
+            hints.reflection().registerType( ChangeLogHistoryServiceFactory.class, (type) ->
+                    type.withConstructor( Collections.emptyList(), ExecutableMode.INVOKE ) );
 
             hints.resources().registerPattern("db/changelog/*" );
 
